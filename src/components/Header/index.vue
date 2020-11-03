5035ff8c3d03ef7303db4021e1a92cc461c9a481
enheng1238 <223547544@qq.com>
2020-11-03 18:19:42 +0800

    2.Header提交

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -11,10 +11,10 @@
                     </div>
                     <!-- 右侧 -->
                     <div class="right">
-                        <router-link to="/login" @click="toLogin">你好，请登录</router-link>
-                        <!-- <a href="/login" class="hover" @click="toLogin" >你好，请登录</a> -->
-                        <!-- <a href="###" class="iconfontC">免费注册</a> -->
-                        <router-link to="/register" class="iconfontC">免费注册</router-link>
+                        <!-- <router-link to="/login" @click="toLogin">你好，请登录</router-link> -->
+                        <a href="/login" class="hover" >你好，请登录</a>
+                        <a href="###" class="iconfontC">免费注册</a>
+                        <!-- <router-link to="/register" class="iconfontC">免费注册</router-link> -->
                         <span class="seprate">|</span>
                         <a href="###">
                             个人中心
@@ -62,6 +62,7 @@ export default {
 
             a{
              text-decoration: none;
+             color: #333;
              }
 
             .container {
