6964a55b7da5977711ee4578527fe96e6425ccde
enheng1238 <223547544@qq.com>
2020-11-03 19:51:49 +0800

    4.Font

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <div>
     <Header></Header>
     <Main></Main>
+    <Font></Font>
   </div>
 </template>
 
 <script>
 import Header from '@/components/Header'
 import Main from '@/components/Main'
+import Font from '@/components/Font'
 export default {
   name: 'APP',
   components:{
     Header,
-    Main
+    Main,
+    Font
   }
 }
 </script>
