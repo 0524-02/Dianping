77392f4e7c99a689573c020f1ae1cc9e3dd5a4f3
enheng1238 <223547544@qq.com>
2020-11-04 08:44:32 +0800

    day02提交

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,21 @@
 <template>
   <div>
     <Header></Header>
-    <!-- <Main></Main> -->
-    <Logo></Logo>
-    <!-- <Footer></Footer> -->
-    <Font></Font>
+    <Main></Main>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 import Header from '@/components/Header'
-// import Main from '@/components/Main'
-import Logo from '@/components/Logo'
-import Font from '@/components/Font'
-// import Footer from '@/components/Footer'
+import Main from '@/components/Main'
+import Footer from '@/components/Footer'
 export default {
   name: 'APP',
   components:{
     Header,
-    // Main,
-    Logo,
-    Font,
-    // Footer
+    Main,
+    Footer
   }
 }
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,11 @@
-//module.exports 是commonJS的写法
 module.exports = {
-    devServer: {
-      lintOnSave: false,
-        proxy: {
-          '/api': {
-            target: 'http://localhost:3003',
-            ws: true,
-            changeOrigin: true,
-                      pathRewrite: { // 路径重写
-                          "^/api": ''
-                      }
+  lintOnSave: false,
+  devServer:{
+      proxy: {
+          "/api": {
+            target: "http://182.92.128.115/",
+            pathRewrite: {"^/api" : ""}
           }
         }
-      }
   }
+}
